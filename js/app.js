e1935cea334ec02d4bf0b7f82bb5e71f665ff9f5
Danielapariona <daniela.parionac@gmail.com>
2017-12-30 22:37:28 -0500

    Show alert with code LAB

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-$(window).on('load', function () {
-  setTimeout(function () {
-    $('.loader-wrap').fadeOut(500);
-  }, 1000);
+$(document).ready(function() {
+  $(window).on('load', function () {
+    setTimeout(function () {
+      $('.loader-wrap').fadeOut(500);
+    }, 1000);
+  });
+
+  
+  // show a dialog box when clicking on an element
+  var $btnNext = $('#next');
+  $btnNext.on('click', function(e) {
+      e.preventDefault();
+      alert('Tú código: LAB-'+ numRandom());
+  });
 });
+
+
+function numRandom() {
+  var $min = 100;
+  var $max = 999;
+  var num = Math.round(Math.random() * ($max - $min) + $min);
+  return num;
+}
