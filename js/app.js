1339a3c4c6b6f35497c764ce280c02f924e475c2
Danielapariona <daniela.parionac@gmail.com>
2017-12-30 23:16:12 -0500

    Redirect when accepting alert

--- js/app.js
+++ js/app.js
@@ -1,19 +1,23 @@
-$(document).ready(function() {
-  $(window).on('load', function () {
-    setTimeout(function () {
-      $('.loader-wrap').fadeOut(500);
-    }, 1000);
-  });
-
-  
-  // show a dialog box when clicking on an element
-  var $btnNext = $('#next');
-  $btnNext.on('click', function(e) {
-      e.preventDefault();
-      alert('Tú código: LAB-'+ numRandom());
-  });
+$(document).ready(function () {
+  //Eventos
+  $(window).on('load', viewSplash);
+  $('#input-text').on('input', validatePhone);
+  $('#next').on('click', showCode);
 });
 
+function validatePhone() {
+  if ($(this).val().trim().length === 10) {
+    $('#next').removeClass('btn-disabled').addClass('btn-active');
+  } else {
+    $('#next').removeClass('btn-active').addClass('btn-disabled')
+  }
+}
+
+function viewSplash() {
+  setTimeout(function () {
+    $('.loader-wrap').fadeOut(500);
+  }, 1000);
+}
 
 function numRandom() {
   var $min = 100;
@@ -21,3 +25,12 @@ function numRandom() {
   var num = Math.round(Math.random() * ($max - $min) + $min);
   return num;
 }
+
+function showCode() {
+  if ($('#next').hasClass('btn-active')) {
+    alert('Tú código: LAB-' + numRandom());
+    //location.reload(true);
+    location.href = 'phoneNumber.html'
+  }
+}
+
