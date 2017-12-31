47ed2788cdf1f1816cad58c9b5b9852e2f29d532
Danielapariona <daniela.parionac@gmail.com>
2017-12-31 02:42:15 -0500

    Refactor functions

--- js/app.js
+++ js/app.js
@@ -2,23 +2,26 @@ $(document).ready(function () {
   //Eventos
   $(window).on('load', viewSplash);
   $('#input-phone').on('input', validatePhone);
-  $('#next').on('click', showCode);
+  $('#signup button').on('click', showCode);
+  $('#resend-code').on('click', resendCode);
+  $('#code').on('input', verifyPhone);
+  $('#verify-number button').on('click', form);
 });
-
-function validatePhone() {
-  if ($(this).val().trim().length === 10) {
-    $('#input-phone').removeClass('input-disable').addClass('input-active');
-    $('#next').removeClass('btn-disabled').addClass('btn-active');
-  } else {
-    $('#input-phone').removeClass('input-active').addClass('input-disable');
-    $('#next').removeClass('btn-active').addClass('btn-disabled');
-  }
-}
 
 function viewSplash() {
   setTimeout(function () {
     $('.loader-wrap').fadeOut(500);
   }, 1000);
+}
+
+function validatePhone() {
+  if ($(this).val().trim().length === 10) {
+    $('#input-phone').removeClass('input-disable').addClass('input-active');
+    $('button#next').removeClass('btn-disabled').addClass('btn-active').removeAttr('disabled');
+  } else {
+    $('#input-phone').removeClass('input-active').addClass('input-disable');
+    $('button#next').removeClass('btn-active').addClass('btn-disabled');
+  }
 }
 
 function numRandom() {
@@ -28,10 +31,28 @@ function numRandom() {
   return num;
 }
 
-function showCode() {
-  if ($('#next').hasClass('btn-active')) {
+function showCode(e) {
+  e.preventDefault();
+  if ($('#signup button').hasClass('btn-active')) {
     alert('Tú código: LAB-' + numRandom());
     //location.reload(true);
     location.href = 'phoneNumber.html'
   }
+}
+
+function resendCode() {
+  alert('Tú código: LAB-' + numRandom());
+}
+
+function verifyPhone() {
+  if ($(this).val().trim().length == 3) {
+    $('#verify-number button').removeClass('btn-disabled').addClass('btn-active').removeAttr('disabled');
+  } else {
+    $('#verify-number button').removeClass('btn-active').addClass('btn-disabled').attr('disabled');
+  }
+}
+
+function form(e) {
+  e.preventDefault();
+  location.href = 'form.html'
 }
